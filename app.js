b9b890a536b0267f94bf701f1f78526f7f82318e
rahilansari261 <rahilansari261@gmail.com>
2023-05-15 12:13:28 +0530

    cors added

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const app = express()
-const users = require('./routes/users')
-const accounts = require('./routes/accounts')
-const products = require('./routes/products')
-const clients = require('./routes/clients')
-const invoices = require('./routes/invoices')
-const connectDB = require('./db/connect')
-require('dotenv').config()
-const notFound = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const users = require("./routes/users");
+const accounts = require("./routes/accounts");
+const products = require("./routes/products");
+const clients = require("./routes/clients");
+const invoices = require("./routes/invoices");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // variables
-app.set('superSecret', process.env.SECRET)
+app.set("superSecret", process.env.SECRET);
 
 // middleware
-app.use(express.static('./public'))
-app.use(express.json())
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(cors());
 
 // routes
-app.use('/api/v1/users', users)
-app.use('/api/v1/accounts', accounts)
-app.use('/api/v1/products', products)
-app.use('/api/v1/clients', clients)
-app.use('/api/v1/invoices', invoices)
+app.use("/api/v1/users", users);
+app.use("/api/v1/accounts", accounts);
+app.use("/api/v1/products", products);
+app.use("/api/v1/clients", clients);
+app.use("/api/v1/invoices", invoices);
 
-app.use(notFound)
-app.use(errorHandlerMiddleware)
-const port = process.env.PORT || 5001
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5001;
 
 const init = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    )
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-init()
+init();
