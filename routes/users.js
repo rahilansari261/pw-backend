cad05522da5dfb1f809dd5a79a1e47ce63169bf3
rahilansari261 <rahilansari261@gmail.com>
2023-05-30 22:47:13 +0530

    authJWT added to getUser controller

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const authJWT = require('../middleware/authJWT')
-const {
-  getUser,
-  createUser,
-  loginUser,
-  forgotUser,
-  verifyUser,
-  resetUser,
-  updateUser,
-  passwordchangeUser,
-  addtaxUser,
-  removetaxUser,
-} = require('../controllers/users')
+const express = require("express");
+const router = express.Router();
+const authJWT = require("../middleware/authJWT");
+const { getUser, createUser, loginUser, forgotUser, verifyUser, resetUser, updateUser, passwordchangeUser, addtaxUser, removetaxUser } = require("../controllers/users");
 
-router.route('/:id').get(getUser)
-router.route('/register2').post(createUser)
-router.route('/login').post(loginUser)
-router.route('/forgot').post(forgotUser)
-router.route('/verify/:c').get(verifyUser)
-router.route('/reset/:c').post(resetUser)
-router.route('/update').post(authJWT, updateUser)
-router.route('/passwordchange').post(authJWT, passwordchangeUser)
-router.route('/settings/addtax').post(authJWT, addtaxUser)
-router.route('/settings/removetax/:taxId').get(authJWT, removetaxUser)
-module.exports = router
+router.route("/:id").get(authJWT, getUser);
+router.route("/register2").post(createUser);
+router.route("/login").post(loginUser);
+router.route("/forgot").post(forgotUser);
+router.route("/verify/:c").get(verifyUser);
+router.route("/reset/:c").post(resetUser);
+router.route("/update").post(authJWT, updateUser);
+router.route("/passwordchange").post(authJWT, passwordchangeUser);
+router.route("/settings/addtax").post(authJWT, addtaxUser);
+router.route("/settings/removetax/:taxId").get(authJWT, removetaxUser);
+module.exports = router;
