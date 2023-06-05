18713b6d4f2657d01308a01239956c4689d87042
rahilansari261 <rahilansari261@gmail.com>
2023-06-05 19:15:52 +0530

    await keyword added before async func

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -166,9 +166,9 @@ const createInvoice = async (req, res) => {
   // prettier-ignore
   if (!userData) return res.status(200).json({ message: error, data: null, success: false })
   // prettier-ignore
-  saleChart(newInvoice.invoice_data.date, newInvoice.invoice_data.grand_total, ChartSaleCollection)
+ await saleChart(newInvoice.invoice_data.date, newInvoice.invoice_data.grand_total, ChartSaleCollection)
   // prettier-ignore
-  updateAccounts(newInvoice, AccountCollection, ClientCollection, invoiceData.invoice_data.advancePayment, InvoiceCollection)
+ await updateAccounts(newInvoice, AccountCollection, ClientCollection, invoiceData.invoice_data.advancePayment, InvoiceCollection)
   // prettier-ignore
   res.status(200).json({message: 'invoice Added Successfully',data: doc, success: true,})
 }
@@ -214,9 +214,9 @@ const cancelInvoice = async (req, res) => {
   // prettier-ignore
   if (!invoiceData) return res.status(200).json({ message: error, data: null, success: false })
   // prettier-ignore
-  saleChart(invoiceData.invoice_data.date, -invoiceData.invoice_data.grand_total, ChartSaleCollection)
+  await saleChart(invoiceData.invoice_data.date, -invoiceData.invoice_data.grand_total, ChartSaleCollection)
   // prettier-ignore
-  updateCancelAccounts(invoiceData, AccountCollection, ClientCollection)
+  await updateCancelAccounts(invoiceData, AccountCollection, ClientCollection)
   // prettier-ignore
   res.status(200).json({message: 'invoice Cancelled  Successfully',data: doc, success: true,})
 }
