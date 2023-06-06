0c8ff5fbb4e7c9ccd42dddf82dda7ed41f757ced
rahilansari261 <rahilansari261@gmail.com>
2023-06-06 15:24:45 +0530

    bug fixes

--- routes/clients.js
+++ routes/clients.js
@@ -1,23 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const authJWT = require('../middleware/authJWT')
-const {
-  createClient,
-  updateClient,
-  removeClient,
-  getClientDetail,
-  getAllClients
-  getClientWithSearchAndPaging,
-  createClientAccounts,
-} = require('../controllers/clients')
+const express = require("express");
+const router = express.Router();
+const authJWT = require("../middleware/authJWT");
+const { createClient, updateClient, removeClient, getClientDetail, getAllClients, getClientWithSearchAndPaging, createClientAccounts } = require("../controllers/clients");
 
-router.route('/add').post(authJWT,createClient)
-router.route('/update').post(authJWT,updateClient)
-router.route('/:id').get(authJWT,getClientDetail)
-router.route('/all').get(authJWT,getAllClients)
-router.route('/remove/:id').get(authJWT,removeClient)
-router.route('/:page/:perPage/:searchStr').get(authJWT,getClientWithSearchAndPaging)
-router.route('/sorted/:page/:perPage/:searchStr').get(authJWT,getClientWithSearchAndPaging)
-router.route('/accounts').post(authJWT,createClientAccounts)
+router.route("/add").post(authJWT, createClient);
+router.route("/update").post(authJWT, updateClient);
+router.route("/:id").get(authJWT, getClientDetail);
+router.route("/all").get(authJWT, getAllClients);
+router.route("/remove/:id").get(authJWT, removeClient);
+router.route("/:page/:perPage/:searchStr").get(authJWT, getClientWithSearchAndPaging);
+router.route("/sorted/:page/:perPage/:searchStr").get(authJWT, getClientWithSearchAndPaging);
+router.route("/accounts").post(authJWT, createClientAccounts);
 
-module.exports = router
+module.exports = router;
