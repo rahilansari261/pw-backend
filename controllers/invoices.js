86cabd26df947294568647cb2d7c9f6993fc59a5
rahilansari261 <rahilansari261@gmail.com>
2023-06-06 11:29:51 +0530

    msg added

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -220,6 +220,7 @@ const cancelInvoice = async (req, res) => {
     // prettier-ignore
     res.status(200).json({message: 'invoice Cancelled  Successfully',data: doc, success: true,})
   } catch (error) {
+    console.log(error)
     res.status(200).json({ message: error.msg, data: null, success: false });
   }
 };
