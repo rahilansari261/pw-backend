63814951bac71a0fdadec8692367f743dce5dc60
rahilansari261 <rahilansari261@gmail.com>
2023-06-10 17:15:51 +0530

    new added in path create invoice path

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const router = express.Router()
-const authJWT = require('../middleware/authJWT')
+const express = require("express");
+const router = express.Router();
+const authJWT = require("../middleware/authJWT");
 const {
   createInvoice,
   getInvoiceDetail,
@@ -14,24 +14,18 @@ const {
   getAllInvoices,
   getAllCancelledInvoices,
   getAllInvoicesAccoToSearch,
-} = require('../controllers/invoices')
+} = require("../controllers/invoices");
 
-router.route('/add').post(authJWT,createInvoice)
-router.route('/:id').get(authJWT,getInvoiceDetail)
-router.route('/recent').get(authJWT,getRecentInvoiceDetail)
-router.route('/cancel/:id').get(authJWT,cancelInvoice)
-router.route('/unpaid/:client_id').get(authJWT,getUnpaidInvoice)
-router.route('/:page/:perPage/:type').get(authJWT,getPaginatedInvoicesAccoToType)
-router
-  .route('/:page/:perPage/:type/:searchStr')
-  .get(authJWT,getPaginatedInvoicesAccoToTypeAndSearch)
-router
-  .route('/report/:start_date/:end_date')
-  .get(authJWT,getInvoicesReportWithDateFilter)
-router
-  .route('/:page/:perPage/:start_date/:end_date')
-  .get(authJWT,getPaginatedInvoicesWithDateFilter)
-router.route('/getall').get(authJWT,getAllInvoices)
-router.route('/cancelled').get(authJWT,getAllCancelledInvoices)
-router.route('/:searchStr').get(authJWT,getAllInvoicesAccoToSearch)
-module.exports = router
+router.route("/new/add").post(authJWT, createInvoice);
+router.route("/:id").get(authJWT, getInvoiceDetail);
+router.route("/recent").get(authJWT, getRecentInvoiceDetail);
+router.route("/cancel/:id").get(authJWT, cancelInvoice);
+router.route("/unpaid/:client_id").get(authJWT, getUnpaidInvoice);
+router.route("/:page/:perPage/:type").get(authJWT, getPaginatedInvoicesAccoToType);
+router.route("/:page/:perPage/:type/:searchStr").get(authJWT, getPaginatedInvoicesAccoToTypeAndSearch);
+router.route("/report/:start_date/:end_date").get(authJWT, getInvoicesReportWithDateFilter);
+router.route("/:page/:perPage/:start_date/:end_date").get(authJWT, getPaginatedInvoicesWithDateFilter);
+router.route("/getall").get(authJWT, getAllInvoices);
+router.route("/cancelled").get(authJWT, getAllCancelledInvoices);
+router.route("/:searchStr").get(authJWT, getAllInvoicesAccoToSearch);
+module.exports = router;
