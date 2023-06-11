b7b447051945c109af919659fff97e5d9434ae17
rahilansari261 <rahilansari261@gmail.com>
2023-06-11 18:47:36 +0530

    api bug fixes

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
-const passwordHash = require('password-hash')
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const passwordHash = require("password-hash");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 // const asyncWrapper = require('../middleware/async')
 // const { createCustomError } = require('../errors/custom-error')
-const { dateFilter } = require('../utils/utils')
+const { dateFilter } = require("../utils/utils");
 // <!-- two api remaining getAll and 1 in getAccountDetails function that is commented -->
 
 const updateInvoices = async (accountData, InvoiceCollection) => {
@@ -13,23 +13,23 @@ const updateInvoices = async (accountData, InvoiceCollection) => {
       { _id: accountData.invoice_list[i]._id },
       {
         $inc: {
-          'invoice_data.balance': accountData.invoice_list[i].amount * -1,
+          "invoice_data.balance": accountData.invoice_list[i].amount * -1,
         },
         $push: {
-          'invoice_data.paymentHistory': {
+          "invoice_data.paymentHistory": {
             dated: new Date(),
             amount: accountData.invoice_list[i].amount,
             remark: accountData.entry_transaction_number,
           },
         },
       }
-    )
+    );
   }
-}
+};
 
 const createAccount = async (req, res) => {
   try {
-    const accountData = req.body.accountData
+    const accountData = req.body.accountData;
     // prettier-ignore
     if (!accountData) return res.status(200).json({ message: ' Data Not Found', data: null, success: false })
     // prettier-ignore
@@ -49,74 +49,51 @@ const createAccount = async (req, res) => {
       entry_amount_in: accountData.entry_amount_in,
       entry_amount_out: accountData.entry_amount_out,
       // "entry_balance": accountData.entry_balance
-    }
-    const doc = await AccountCollection.create(newAccount)
+    };
+    const doc = await AccountCollection.create(newAccount);
     // prettier-ignore
     if (!doc) return res.status(200).json({ message: error, data: null, success: false })
     else {
       // prettier-ignore
       await ClientCollection.updateOne({ _id: accountData.client_id },{ $inc: { client_balance: accountData.entry_amount_in * -1 } })
     }
-    if (accountData.entry_amount_in > 0)
-      await updateInvoices(accountData, InvoiceCollection)
+    if (accountData.entry_amount_in > 0) await updateInvoices(accountData, InvoiceCollection);
     // prettier-ignore
     return res.status(200).json({message: 'Account Update Successfully',data: doc,success: true})
   } catch (error) {
     // prettier-ignore
     res.status(200).json({message: error,success: false,})
   }
-}
-const getFindAndSortOptionsAccoToParams = async (
-  client_id,
-  searchString,
-  start_date,
-  end_date
-) => {
-  let findAndSortOptions = {}
+};
+const getFindAndSortOptionsAccoToParams = async (client_id, searchString, start_date, end_date) => {
+  let findAndSortOptions = {};
   // if (start_date != 'no-start-date' && end_date != 'no-end-date') {
   //   let { startDate, endDate } = await dateFilter(start_date, end_date)
   // }
   // 1-> getting accounts with date filter and paging
-  if (
-    client_id === 'no-id' &&
-    searchString === 'no-search' &&
-    start_date != 'no-start-date' &&
-    end_date != 'no-end-date'
-  ) {
-    const { startDate, endDate } = await dateFilter(start_date, end_date)
+  if (client_id === "no-id" && searchString === "no-search" && start_date != "no-start-date" && end_date != "no-end-date") {
+    const { startDate, endDate } = await dateFilter(start_date, end_date);
     findAndSortOptions.findOptions = {
       entry_date: {
         $gte: startDate,
         $lt: endDate,
       },
-    }
+    };
     findAndSortOptions.sortOptions = {
       _id: -1,
-    }
+    };
   }
   // 2-> getting accounts of a client(client_id) and with search string and paging
-  else if (
-    start_date === 'no-start-date' &&
-    end_date === 'no-end-date' &&
-    client_id != 'no-id' &&
-    searchString != 'no-search'
-  ) {
+  else if (start_date === "no-start-date" && end_date === "no-end-date" && client_id != "no-id" && searchString != "no-search") {
     findAndSortOptions.findOptions = {
-      $and: [
-        { entry_remarks: new RegExp(req.params.searchString, 'i') },
-        { client_id: id },
-      ],
-    }
-    findAndSortOptions.sortOptions = { entry_date: -1 }
+      $and: [{ entry_remarks: new RegExp(req.params.searchString, "i") }, { client_id: id }],
+    };
+    findAndSortOptions.sortOptions = { entry_date: -1 };
   }
   // 3-> getting accounts of a client(client_id) and paging
-  else if (
-    start_date === 'no-start-date' &&
-    end_date === 'no-end-date' &&
-    client_id != 'no-id'
-  ) {
-    findAndSortOptions.findOptions = { client_id: id }
-    findAndSortOptions.sortOptions = { entry_date: -1 }
+  else if (start_date === "no-start-date" && end_date === "no-end-date" && client_id != "no-id") {
+    findAndSortOptions.findOptions = { client_id: client_id };
+    findAndSortOptions.sortOptions = { entry_date: -1 };
   }
   // 4-> getting accounts of a client(client_id) with date filter and paging  (TYPICAL)
   // else if (
@@ -135,71 +112,56 @@ const getFindAndSortOptionsAccoToParams = async (
   // }
 
   // 5-> getting accounts with search filter and paging
-  else if (
-    start_date === 'no-start-date' &&
-    end_date === 'no-end-date' &&
-    client_id === 'no-id' &&
-    searchString != 'no-search'
-  ) {
+  else if (start_date === "no-start-date" && end_date === "no-end-date" && client_id === "no-id" && searchString != "no-search") {
     findAndSortOptions.findOptions = {
       $and: [
         {
           $or: [
             {
-              account_name: new RegExp(req.params.searchStr, 'i'),
+              account_name: new RegExp(req.params.searchStr, "i"),
             },
             {
-              account_company: new RegExp(req.params.searchStr, 'i'),
+              account_company: new RegExp(req.params.searchStr, "i"),
             },
           ],
         },
       ],
-    }
-    findAndSortOptions.sortOptions = { entry_date: -1 }
+    };
+    findAndSortOptions.sortOptions = { entry_date: -1 };
   }
 
   // 6-> getting accounts of a client(client_id) with search and date filter and paging
-  else if (
-    start_date != 'no-start-date' &&
-    end_date != 'no-end-date' &&
-    client_id != 'no-id' &&
-    searchString != 'no-search'
-  ) {
-    const { startDate, endDate } = await dateFilter(start_date, end_date)
+  else if (start_date != "no-start-date" && end_date != "no-end-date" && client_id != "no-id" && searchString != "no-search") {
+    const { startDate, endDate } = await dateFilter(start_date, end_date);
     findAndSortOptions.findOptions = {
       $and: [
         {
-          $or: [{ entry_remarks: new RegExp(req.params.searchString, 'i') }],
+          $or: [{ entry_remarks: new RegExp(req.params.searchString, "i") }],
         },
         { entry_date: { $gte: startDate, $lte: endDate } },
         { client_id: id },
       ],
-    }
-    findAndSortOptions.sortOptions = { entry_date: -1 }
+    };
+    findAndSortOptions.sortOptions = { entry_date: -1 };
   }
   // 7-> getting accounts with no search filter and paging
-  else if (
-    start_date === 'no-start-date' &&
-    end_date === 'no-end-date' &&
-    client_id === 'no-id' &&
-    searchString === 'no-search'
-  ) {
-    console.log('yaha pr hu')
-    findAndSortOptions.findOptions = {}
-    findAndSortOptions.sortOptions = { entry_date: -1 }
+  else if (start_date === "no-start-date" && end_date === "no-end-date" && client_id === "no-id" && searchString === "no-search") {
+    console.log("yaha pr hu");
+    findAndSortOptions.findOptions = {};
+    findAndSortOptions.sortOptions = { entry_date: -1 };
   }
-  return findAndSortOptions
-}
+  return findAndSortOptions;
+};
 const getAccountDetails = async (req, res) => {
-  console.log('numuste')
+  console.log("numuste");
   try {
     // prettier-ignore
     let { client_id, searchString, page, perPage, start_date, end_date } = req.params
     // prettier-ignore
     if (isNaN(page) || isNaN(perPage)) return res.status(200).json({message: 'Pagin Error',data: null,success: false,})
-    page = parseInt(page)
-    perPage = parseInt(perPage)
-    const startingPageForSort = (page - 1) * perPage
+    page = parseInt(page);
+    perPage = parseInt(perPage);
+    const startingPageForSort = (page - 1) * perPage;
     // prettier-ignore
     const AccountCollection = mongoose.model(`${req.doc._id}-accounts`, require('../models/Account' ) )
     // prettier-ignore
@@ -211,13 +173,13 @@ const getAccountDetails = async (req, res) => {
     // prettier-ignore
     res.status(200).json({message: 'Accounts after search',count: docs.length,data: docs,success: true,})
   } catch (error) {
-    console.log(error)
+    console.log(error);
     // prettier-ignore
-    res.status(200).json({message: error,success: false})
+    res.status(200).json({message: error.msg,success: false})
   }
-}
+};
 
 module.exports = {
   createAccount,
   getAccountDetails,
-}
+};
