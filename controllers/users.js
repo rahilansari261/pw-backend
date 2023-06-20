44685913f415192b587a71b97aecf31e0ebd79f5
rahilansari261 <rahilansari261@gmail.com>
2023-06-20 18:04:56 +0530

    bug fixes

--- controllers/users.js
+++ controllers/users.js
@@ -312,7 +312,7 @@ const addTandC = async (req, res) => {
 
 const salesGraph = async (req, res) => {
   try {
-    const ChartSaleCollection = mongoose.model(`${req.doc._id}-saledatas`, require("../models/ChartSale"));
+    const ChartSaleCollection = mongoose.model(`${req.doc._id}-salesdatas`, require("../models/ChartSale"));
     // prettier-ignore
     const doc = await ChartSaleCollection.find({})
     // prettier-ignore
